dbec8c00f7df57f83314b3e1a11e4dd82f0ee43d
aa6 <justsunshineandbluesky@gmail.com>
2019-04-01 13:40:09 +0300

    test123

--- alpine.js
+++ alpine.js
@@ -5,7 +5,6 @@
  * Created by blarsen on 02.10.14.
  */
 
-"use strict";
 
 var Buffer = require('./buffer');
 
@@ -49,8 +48,8 @@ function getStringStream() {
 
 function parseReadStream(stream, callback) {
     var thisAlpine = this;
-    var lineStream = byline.createStream(stream);
-    lineStream.pipe(through2.obj(function(chunk, enc, t2callback) {
+    var stream = byline.createStream(stream);
+    stream.pipe(through2.obj(function(chunk, enc, t2callback) {
         var data = thisAlpine.parseLine(chunk.toString());
         callback(data);
         t2callback();
@@ -81,18 +80,18 @@ function parseLine(line) {
                 throw new Error("Field defined as quoted was not quoted");
             buf.skip();
             val = buf.getUpto('"');
-            buf.skipSpaces();
+            buf.skip();
         } else if (field.isDate) {
             if (!(buf.lookingAt() === '['))
                 throw new Error("Time field is not enclosed in brackets");
             buf.skip();
             val = buf.getUpto(']');
-            buf.skipSpaces();
+            buf.skip();
         } else {
             val = buf.getUpto(' ');
         }
         result[field.name] = val;
-    });
+    })
 
     return result;
 }
@@ -104,7 +103,7 @@ function parseLogFormat(logformat) {
         buf.skipSpaces();
         var field = buf.getUpto(" ");
         var isQuoted = field[0] === '"';
-        field = stripQuotes(field);
+        var field = stripQuotes(field);
 
         // Check that this is a field definition (starting with %) and remove the prefix
         if (!(field[0] === "%"))
@@ -113,31 +112,29 @@ function parseLogFormat(logformat) {
 
         // Remove modifiers
         if (field.indexOf("{") > 0) {
-            field = field.replace(/^[0-9!]+/, "");
+            field = field.replace(/^[0-9!]+//g, "");
         }
         field = field.replace(/[<>]/g, "");
 
         var fieldName = FIELDS[field];
 
-        var fieldLetter = field;
-
         // Handle parameterized fields
         if (field.indexOf('{') >= 0) {
             var matches = (/{(.*)}(.*)/).exec(field);
             var value = matches[1];
-            fieldLetter = matches[2];
-            if (!PARAMFIELDS[fieldLetter])
-                throw new Error("The field "+fieldLetter+" should not be parameterized");
-            fieldName = PARAMFIELDS[fieldLetter] + ' ' + value;
+            var field = matches[2];
+            if (!PARAMFIELDS[field])
+                throw new Error("The field "+field+" should not be parameterized");
+            fieldName = PARAMFIELDS[field] + ' ' + value;
         }
 
-        if (!FIELDS[fieldLetter])
-            throw new Error("Unknown log format field "+fieldLetter);
+        if (!FIELDS[field])
+            throw new Error("Unknown log format field "+field);
         fields.push({
             field: field,
             name: fieldName,
             isQuoted: isQuoted,
-            isDate: fieldLetter === 't'
+            isDate: field === 't'
         });
     }
     return fields;
@@ -154,7 +151,7 @@ Alpine.LOGFORMATS = {
     COMBINED: "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"",
     CLF: "%h %l %u %t \"%r\" %>s %b",
     CLF_VHOST: "%v %h %l %u %t \"%r\" %>s %b"
-};
+}
 
 var FIELDS = {
     'a': 'remoteIP',
@@ -188,11 +185,13 @@ var FIELDS = {
     'i': 'requestHeader',
     'n': 'note',
     'o': 'responseHeader',
+    'p': 'formatPort',
+    'P': 'pidFormat',
     '^ti': 'requestTrailerLine',
     '^to': 'responseTrailerLine'
-};
+}
 
-var PARAMFIELDS = {
+PARAMFIELDS = {
     "c": "Cookie",
     "e": "Environment",
     "i": "RequestHeader",
@@ -203,6 +202,6 @@ var PARAMFIELDS = {
     "t": "Time",
     '^ti': 'RequestTrailerLine',
     '^to': 'ResponseTrailerLine'
-};
+}
 
 module.exports = Alpine;
